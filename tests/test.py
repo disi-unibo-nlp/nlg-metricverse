b0c89ee1c3901df5ce95c3836d814c238a796016
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2022-03-09 14:16:22 +0100

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,7 @@
 from nlgmetricverse import Nlgmetricverse
 
 scorer = Nlgmetricverse()
-
+'''
 predictions_str = [
     ["the cat is on the mat", "There is cat playing on the mat"],
     ["Look!    a wonderful day."]
@@ -10,16 +10,20 @@ references_str = [
     ["the cat is playing on the mat.", "The cat plays on the mat."],
     ["Today is a wonderful day", "The weather outside is wonderful."]
 ]
-
+'''
 with open("predictions.txt", "r") as file:
     predictions_txt = file.readlines()
-
+print("Predictions:")
 print(predictions_txt)
 
 with open('references.txt', 'r') as file:
     references_txt = file.readlines()
-
+print("References:")
 print(references_txt)
-
+print()
+'''
+scores = scorer(predictions=predictions_str, references=references_str)
+'''
 scores = scorer(predictions=predictions_txt, references=references_txt)
+print("\nScores:")
 print(scores)
