bb1e4786e1cd1d1dffdf3e5b303e64f60fcfdcbe
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2022-03-17 20:53:29 +0100

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -8,9 +8,11 @@ scores = scorer(predictions=predictions_str, references=references_str)
 '''
 predictions = os.getcwd() + "/predictions"
 references = os.getcwd() + "/references"
+
 scores = scorer(predictions=predictions, references=references, method="read_lines")
-print("\nScores:")
+print("\nScores with method 'read_lines':")
 print(scores)
+
 scores = scorer(predictions=predictions, references=references, method="no_new_line")
-print("\nScores:")
+print("\nScores with method 'no_new_line':")
 print(scores)
