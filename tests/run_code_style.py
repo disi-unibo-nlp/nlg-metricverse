5bc977fb0d01432dd77cb2358edfaa3301210ffd
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2022-04-20 14:58:19 +0200

    Update run_code_style.py

--- tests/run_code_style.py
+++ tests/run_code_style.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     arg = sys.argv[1]
 
     if arg == "check":
-        sts_flake = shell("flake8 nlg-metricverse tests --config setup.cfg")
+        sts_flake = shell("flake8 nlgmetricverse tests --config setup.cfg")
         sts_isort = shell("isort . --check --settings setup.cfg")
         sts_black = shell("black . --check --config pyproject.toml")
         validate_and_exit(flake8=sts_flake, isort=sts_isort, black=sts_black)
