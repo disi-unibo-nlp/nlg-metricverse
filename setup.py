fa490099bc67c5b895c43e04e67af01a9501b138
DISI UniBo NLP <74385848+disi-unibo-nlp@users.noreply.github.com>
2022-04-22 20:01:36 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,56 @@
+# Lint as: python3
+""" NLG Metricverse is an open library of metrics for evaluating artificial text.
+
+Note:
+   VERSION needs to be formatted following the MAJOR.MINOR.PATCH convention
+   (we need to follow this convention to be able to retrieve versioned scripts)
+   
+To create the package for pypi.
+
+0. Prerequisites:
+   - Dependencies:
+     - twine: "pip install twine"
+   - Create an account in (and join the 'nlg-metricverse' project):
+     - PyPI: https://pypi.org/
+     - Test PyPI: https://test.pypi.org/
+1. Change the version in:
+   - __init__.py
+   - setup.py
+
+2. Commit these changes: "git commit -m 'Release: VERSION'"
+
+3. Add a tag in git to mark the release: "git tag VERSION -m 'Add tag VERSION for pypi'"
+   Push the tag to remote: git push --tags origin master
+
+4. Build both the sources and the wheel. Do not change anything in setup.py between
+   creating the wheel and the source distribution (obviously).
+   
+   First, delete any "build" directory that may exist from previous builds.
+   
+   For the wheel, run: "python setup.py bdist_wheel" in the top level directory.
+   (this will build a wheel for the python version you use to build it).
+   
+   For the sources, run: "python setup.py sdist"
+   You should now have a /dist directory with both .whl and .tar.gz source versions.
+
+5. Check that everything looks correct by uploading the package to the pypi test server:
+
+   twine upload dist/* -r pypitest --repository-url=https://test.pypi.org/legacy/
+   
+   Check that you can install it in a virtualenv/notebook by running:
+   pip install huggingface_hub fsspec aiohttp
+   pip install -U tqdm
+   pip install -i https://testpypi.python.org/pypi datasets
+   
+6. Upload the final version to actual pypi:
+   twine upload dist/* -r pypi
+   
+7. Fill release notes in the tag in github once everything is looking hunky-dory.
+
+8. Change the version in __init__.py and setup.py to X.X.X+1.dev0 (e.g. VERSION=1.18.3 -> 1.18.4.dev0).
+   Then push the change with a message 'set dev version'
+"""
+
 import io
 import os
 import platform
