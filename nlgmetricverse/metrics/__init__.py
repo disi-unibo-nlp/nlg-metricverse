934c41397ea1356ebe688da91ea67484a645cecf
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2022-05-13 16:53:38 +0200

    Update __init__.py

--- nlgmetricverse/metrics/__init__.py
+++ nlgmetricverse/metrics/__init__.py
@@ -7,17 +7,22 @@ from nlgmetricverse.metrics._core import (
     list_metrics,
     load_metric,
 )
+from nlgmetricverse.metrics.accuracy import Accuracy
 from nlgmetricverse.metrics.bartscore import Bartscore
 from nlgmetricverse.metrics.bertscore import Bertscore
 from nlgmetricverse.metrics.bleu import Bleu
 from nlgmetricverse.metrics.bleurt import Bleurt
+from nlgmetricverse.metrics.cer import CER
 from nlgmetricverse.metrics.chrf import CHRF
 from nlgmetricverse.metrics.comet import Comet
+from nlgmetricverse.metrics.f1 import F1
 from nlgmetricverse.metrics.meteor import Meteor
 from nlgmetricverse.metrics.moverscore import Moverscore
 from nlgmetricverse.metrics.nist import Nist
+from nlgmetricverse.metrics.nubia import Nubia
+from nlgmetricverse.metrics.precision import Precision
+from nlgmetricverse.metrics.prism import Prism
 from nlgmetricverse.metrics.rouge import Rouge
 from nlgmetricverse.metrics.sacrebleu import Sacrebleu
 from nlgmetricverse.metrics.ter import TER
 from nlgmetricverse.metrics.wer import WER
-from nlgmetricverse.metrics.nubia import Nubia
