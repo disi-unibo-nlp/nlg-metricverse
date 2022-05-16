f1b9ee1d2177820d8deba06498ac4f9a84500ebf
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2022-05-16 15:59:17 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -111,7 +111,7 @@ _DEV_REQUIREMENTS = [
     "torch==1.10.0",
     "pytorch-transformers==1.2.0",
     "math_equivalence @ git+https://github.com/hendrycks/math.git",  # for datasets test metric
-    "fairseq>=0.10.0",
+    "fairseq",
     "wget==3.2"
 ]
 
