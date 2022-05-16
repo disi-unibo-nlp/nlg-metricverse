81245ffad9974fe312262f3e9ab19424b3631d98
Giacomo Frisoni <giacomofrisoni@gmail.com>
2022-05-16 15:41:27 +0200

    Update __init__.py

--- nlgmetricverse/__init__.py
+++ nlgmetricverse/__init__.py
@@ -1,4 +1,4 @@
 from nlgmetricverse.core import Nlgmetricverse
 from nlgmetricverse.metrics import AutoMetric, list_metrics, get_metrics_by_task, load_metric
 
-__version__ = "1.1"
+__version__ = "0.1.0"
