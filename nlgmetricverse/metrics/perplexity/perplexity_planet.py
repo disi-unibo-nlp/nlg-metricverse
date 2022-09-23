a6b777fd2159bc70fca35893c5cf6a37b8e94be7
Marco Avagnano <marcoavagnano98@gmail.com>
2022-09-23 11:44:37 +0200

    update perplexity license

--- nlgmetricverse/metrics/perplexity/perplexity_planet.py
+++ nlgmetricverse/metrics/perplexity/perplexity_planet.py
@@ -1,3 +1,21 @@
+# coding=utf-8
+# Copyright 2021 Open Business Software Solutions, The HuggingFace Datasets Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+""" Perplexity metric. The part of this file is adapted from Perplexity implementation
+of datasets package. See
+https://github.com/huggingface/datasets/blob/main/metrics/perplexity/perplexity.py """
+
 import numpy as np
 import torch
 from torch.nn import CrossEntropyLoss
