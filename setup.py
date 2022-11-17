dd5e451dd642c295777981fa16beebed5c5ece96
DISI UniBo NLP <74385848+disi-unibo-nlp@users.noreply.github.com>
2022-11-17 16:22:12 +0000

    Update version 0.9.5

--- setup.py
+++ setup.py
@@ -176,7 +176,7 @@ setuptools.setup(
     long_description=get_long_description(),
     long_description_content_type="text/markdown",
     url='https://github.com/disi-unibo-nlp/nlg-metricverse',
-    download_url='https://github.com/disi-unibo-nlp/nlg-metricverse/archive/refs/tags/0.9.4.tar.gz',
+    download_url='https://github.com/disi-unibo-nlp/nlg-metricverse/archive/refs/tags/0.9.5.tar.gz',
     packages=setuptools.find_packages(exclude=["tests"]),
     python_requires=">=3.7",
     install_requires=requirements, # dependencies NECESSARY to run the project
