0d70acd0700fe94f241e0addfcb4e0243bd29422
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2023-07-25 17:48:53 +0200

    added tests for accuracy metric

---
+++ tests/nlgmetricverse/metrics/test_accuracy.py
@@ -0,0 +1,54 @@
+import pytest
+
+from nlgmetricverse import NLGMetricverse
+from nlgmetricverse.metrics import AutoMetric
+from tests.nlgmetricverse.conftest import get_expected_output
+from tests.utils import assert_almost_equal_dict
+
+
+@pytest.fixture(scope="module")
+def nlgmetricverse_accuracy():
+    metric = AutoMetric.load("accuracy")
+    return NLGMetricverse(metrics=metric)
+
+
+@pytest.fixture
+@get_expected_output(prefix="metrics")
+def output_basic():
+    return output_basic.output
+
+
+@pytest.fixture
+@get_expected_output(prefix="metrics")
+def output_multiple_ref():
+    return output_multiple_ref.output
+
+
+@pytest.fixture
+@get_expected_output(prefix="metrics")
+def output_multiple_pred_multiple_ref():
+    return output_multiple_pred_multiple_ref.output
+
+
+def test_basic_language_generation(
+    predictions, references, nlgmetricverse_accuracy, output_basic
+):
+    scores = nlgmetricverse_accuracy(predictions=predictions, references=references)
+    assert_almost_equal_dict(actual=scores, desired=output_basic)
+
+
+def test_multiple_ref_language_generation(
+    predictions, multiple_references, nlgmetricverse_accuracy, output_multiple_ref
+):
+    scores = nlgmetricverse_accuracy(predictions=predictions, references=multiple_references)
+    assert_almost_equal_dict(actual=scores, desired=output_multiple_ref)
+
+
+def test_multiple_pred_multiple_ref_language_generation(
+    multiple_predictions,
+    multiple_references,
+    nlgmetricverse_accuracy,
+    output_multiple_pred_multiple_ref,
+):
+    scores = nlgmetricverse_accuracy(predictions=multiple_predictions, references=multiple_references)
+    assert_almost_equal_dict(actual=scores, desired=output_multiple_pred_multiple_ref)
