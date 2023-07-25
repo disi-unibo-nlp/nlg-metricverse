345c27a7216cee1c7cb44b4034c74ca47dc753bb
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2023-07-25 17:49:51 +0200

    added test for precision metric

---
+++ tests/nlgmetricverse/metrics/test_precision.py
@@ -0,0 +1,54 @@
+import pytest
+
+from nlgmetricverse import NLGMetricverse
+from nlgmetricverse.metrics import AutoMetric
+from tests.nlgmetricverse.conftest import get_expected_output
+from tests.utils import assert_almost_equal_dict
+
+
+@pytest.fixture(scope="module")
+def nlgmetricverse_precision():
+    metric = AutoMetric.load("precision")
+    return NLGMetricverse(metrics=metric)
+
+
+@pytest.fixture
+@get_expected_output(prefix="metrics")
+def output_basic():
+    return output_basic.output
+
+
+@pytest.fixture
+@get_expected_output(prefix="metrics")
+def output_multiple_ref():
+    return output_multiple_ref.output
+
+
+@pytest.fixture
+@get_expected_output(prefix="metrics")
+def output_multiple_pred_multiple_ref():
+    return output_multiple_pred_multiple_ref.output
+
+
+def test_basic(
+    predictions, references, nlgmetricverse_precision, output_basic
+):
+    scores = nlgmetricverse_precision(predictions=predictions, references=references)
+    assert_almost_equal_dict(actual=scores, desired=output_basic)
+
+
+def test_multiple_ref(
+    predictions, multiple_references, nlgmetricverse_precision, output_multiple_ref
+):
+    scores = nlgmetricverse_precision(predictions=predictions, references=multiple_references)
+    assert_almost_equal_dict(actual=scores, desired=output_multiple_ref)
+
+
+def test_multiple_pred_multiple_ref(
+    multiple_predictions,
+    multiple_references,
+    nlgmetricverse_precision,
+    output_multiple_pred_multiple_ref,
+):
+    scores = nlgmetricverse_precision(predictions=multiple_predictions, references=multiple_references)
+    assert_almost_equal_dict(actual=scores, desired=output_multiple_pred_multiple_ref)
