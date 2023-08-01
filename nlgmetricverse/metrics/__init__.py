b1887991b62d8f28285bb1f446ea83c098a5115e
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2023-08-01 19:16:46 +0200

    updated __init__.py

--- nlgmetricverse/metrics/__init__.py
+++ nlgmetricverse/metrics/__init__.py
@@ -20,11 +20,9 @@ from nlgmetricverse.metrics.bertscore import Bertscore
 from nlgmetricverse.metrics.bleu import Bleu
 from nlgmetricverse.metrics.bleurt import Bleurt
 from nlgmetricverse.metrics.cer import CER
-from nlgmetricverse.metrics.character import CharacTER
 from nlgmetricverse.metrics.chrf import CHRF
 from nlgmetricverse.metrics.cider import Cider
 from nlgmetricverse.metrics.comet import Comet
-from nlgmetricverse.metrics.eed import EED
 from nlgmetricverse.metrics.f1 import F1
 from nlgmetricverse.metrics.flesch_kincaid import FleschKincaid
 from nlgmetricverse.metrics.gunning_fog import GunningFog
