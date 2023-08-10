480c9d7362b914e5efe1a3475858fd331b0b7b66
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2023-08-10 18:07:47 +0200

    fixed definitions.py

--- nlgmetricverse/definitions.py
+++ nlgmetricverse/definitions.py
@@ -1,5 +1,5 @@
 DEFAULT_METRICS = [
-    {"path": "bertscore", "idf": True, "rescale_with_baseline": True, "model_type": "microsoft/deberta-xlarge-mnli"},
+    {"path": "bertscore", "compute_kwargs": {"idf": True, "rescale_with_baseline": True, "model_type": "microsoft/deberta-xlarge-mnli"}},
     {"path": "bartscore"},
     {"path": "meteor"},
     {"path": "rouge"}
