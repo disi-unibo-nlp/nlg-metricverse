dce0f047afb632fe2ea231b76a9b821563b290e6
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2023-08-21 20:06:03 +0200

    changed metric_info

--- nlgmetricverse/utils/metric_info.py
+++ nlgmetricverse/utils/metric_info.py
@@ -1,10 +1,36 @@
-import evaluate
 from typing import List
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import List, Optional, Union
+
+from datasets.features import Features, Value
 
 @dataclass
-class metric_info(evaluate.MetricInfo):
-    """Information about my custom metric."""
+class metric_info:
 
+    description: str
+    citation: str
+    features: Union[Features, List[Features]]
+    inputs_description: str = field(default_factory=str)
     upper_bound: int
     lower_bound: int
+    homepage: str = field(default_factory=str)
+    license: str = field(default_factory=str)
+    codebase_urls: List[str] = field(default_factory=list)
+    reference_urls: List[str] = field(default_factory=list)
+    streamable: bool = False
+    format: Optional[str] = None
+    module_type: str = "metric"
+
+    # Set later by the builder
+    module_name: Optional[str] = None
+    config_name: Optional[str] = None
+    experiment_id: Optional[str] = None
+
+    def __post_init__(self):
+        if self.format is not None:
+            for key, value in self.features.items():
+                if not isinstance(value, Value):
+                    raise ValueError(
+                        f"When using 'numpy' format, all features should be a `datasets.Value` feature. "
+                        f"Here {key} is an instance of {value.__class__.__name__}"
+                    )
